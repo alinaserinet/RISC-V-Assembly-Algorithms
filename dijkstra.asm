b992b82247bafc1fe37416d8fe5c63e024a10d74
Ali Naseri <mo.ali.naseri@gmail.com>
2023-01-14 00:37:11 +0330

    change matrix to non-square matrix

--- dijkstra.asm
+++ dijkstra.asm
@@ -13,27 +13,33 @@ str_TAB:	.asciz	"\t"
 
 .text
 # put base-of-matrix in (s0)
-# put size-of-matrix in (s1)
+# put matrix-rows    in (s1)
+# put matrix-cols    in (s2)
 
 la 	s0, matrix			# base-of-matrix in (s0)
-li 	s1, 5				# size-of-matrix in (s1)
-li	a0, 10
-mv	s1, a0
-li	a1, -1
-jal	initialSquareMatrix
+li 	s1, 1				# matrix-rows in (s1)
+li	s2, 10				# matrix-cols in (s2)
+
+mv	a1, s1
+mv	a1, s2
+li	a0, -1
+jal 	initialMatrix
 mv	s0, a0
 jal	printMatrix
 
 jal	exit
 
 
-# 'size-of-matrix' in (a0)
-# 'default-value' in (a1)
+# 'rows-count' in            (a1)
+# 'cols-count' in            (a2)
+# 'default-value' in         (a0)
+# -------------------------------
 # return: base of new matrix (a0)
-initialSquareMatrix:	
-	mul	t1, a0, a0		# 'items-count'(t1) = 'rows-count'(a0) * 'col-count'(a0)
+initialMatrix:
+	mul	t1, a1, a2		# 'items-count'(t1) = 'rows-count'(a1) * 'col-count'(a2)
 	slli	t0, t1, 2		# 'items-bytes'(t0) = 'items-count'(t1) * 4 'int-size'
 	sub	sp, sp, t0		# reducing stack-pointer by the size of items-byte
+	mv	t3, a0			# copy default-value to (t3)
 	mv	a0, sp			# 'matrix-base'(a0) = stack-pointer
 	
 	# save 'return-address' to stack
@@ -42,13 +48,13 @@ initialSquareMatrix:
 	
 	mv	t0, a0			# 'bytes-counter'(t0) = 'matrix-base'(a0)
 	li	t2, 0			# 'items-counter'(t2) = 0
-	initialSquareMatrix_loop:
-		bge	t2, t1, initialSquareMatrix_end
-		sw	a1, 0(t0)
-		addi	t0, t0, 4
-		addi	t2, t2, 1
-		jal	initialSquareMatrix_loop
-	initialSquareMatrix_end:
+	initialMatrix_loop:
+		bge	t2, t1, initialMatrix_end	# check items-counter(t2) < items-count(t1)
+		sw	t3, 0(t0)			# store default-value to current byte-address
+		addi	t0, t0, 4			# bytes-counter += 4
+		addi	t2, t2, 1			# items-counter += 1
+		jal	initialMatrix_loop		# jump to loop
+	initialMatrix_end:
 	# load 'return-address' from stack
 	lw	ra, 0(sp)
 	addi	sp, sp, 4
@@ -56,15 +62,19 @@ initialSquareMatrix:
 	jalr	zero, 0(ra)		# return - base-of-matrix in (a0)
 	
 
+# 'matrix-base' in (s0)
+# 'matrix-rows' in (s1)
+# 'matrix-cols' in (s2)
 # 'row-index'   in (a0)
 # 'col-index'   in (a1)
+# ----------------------------
 # return: 'edge-value' in (a0)
-getEdge:
+getItem:
 	# save 'return-address' to stack
 	addi	sp, sp, -4
 	sw	ra, 0(sp)
 	
-	jal	getEdgeAdrress 		# 'edge-address' in (a0)
+	jal	getItemAddress 		# 'edge-address' in (a0)
 	
 	# load 'return-address' from stack
 	lw	ra, 0(sp)
@@ -73,67 +83,73 @@ getEdge:
 	lw	a0, 0(a0)		# load 'edge-value' from array[(a0)][(a1)]
 	jalr	zero, 0(ra)		# return 'edge-value'
 
-
+# 'matrix-base' in (s0)
+# 'matrix-rows' in (s1)
+# 'matrix-cols' in (s2)
 # 'row-index'   in (a0)
 # 'col-index'   in (a1)
-# 'edge-value'  in (a2)
-setEdge:
+# 'item-value'  in (a2)
+setItem:
 	# save 'return-address' to stack
 	addi	sp, sp, -4
 	sw	ra, 0(sp)
 	
-	jal	getEdgeAdrress 		# 'edge-address' in (a0)
+	jal	getItemAddress 		# 'item-address' in (a0)
 	
 	# load 'return-address' from stack
 	lw	ra, 0(sp)
 	addi	sp, sp, 4
 	
-	sw	a2, 0(a0) 		# save 'edge-value' in array[(a0)][(a1)]
+	sw	a2, 0(a0) 		# save 'item-value' in array[(a0)][(a1)]
 	jalr	zero, 0(ra) 		# return
 	
 	
+
 # 'matrix-base' in (s0)
-# 'matrix-size' in (s1)
+# 'matrix-rows' in (s1)
+# 'matrix-cols' in (s2)
 # 'row-index'   in (a0)
 # 'col-index'   in (a1)
+# ------------------------------
 # return: 'edge-address' in (a0)
-getEdgeAdrress:
-	bgeu	a0, s1, outOfBound 	# check 'row-index' < 'matrix-size'
-	bgeu	a1, s1, outOfBound 	# check 'col-index' < 'matrix-size'
-	slli	t0, s1, 2		# 'row-size' = `matrix-rows-count` * 4 (int-size)
+getItemAddress:
+	bgeu	a0, s1, outOfBound	# check 'row-index' < 'matrix-rows'
+	bgeu	a1, s2, outOfBound 	# check 'col-index' < 'matrix-cols'
+	slli	t0, s2, 2		# 'row-bytes' = `matrix-cols` * 4 (int-size)
 	mul	t0, t0, a0		# 'row-index' * 'row-size' (to find rows-offset)
 	slli	t1, a1, 2		# 'col-index' * 4 (int-size) (to find cols-offset)
 	add	t0, s0, t0		# address = base + rows-offset
 	add	a0, t0, t1		# address += cols-offset
 	jalr	zero, 0(ra)		# return address
 
-# 'matrix-base'	in (s0)
-# 'matrix-size' in (s1)
+# 'matrix-base' in (s0)
+# 'matrix-rows' in (s1)
+# 'matrix-cols' in (s2)
 printMatrix:
 	# save 'return-address' to stack
 	addi	sp, sp, -4
 	sw	ra, 0(sp)
 	
-	li	t0, 0			# 'row-counter'(t0) = 0
+	li	t0, 0		# 'row-counter'(t0) = 0
 	printMatrix_loop1:
 		# check 'row-counter'(t0) < 'matrix-size'(s1)
 		bgeu	t0, s1, printMatrix_end1
 		li	t1, 0		# 'col-counter' (t1) = 0
 		printMatrix_loop2:
 			# check 'col-counter'(t1) < 'matrix-size'(s1)
-			bgeu	t1, s1, printMatrix_end2
+			bgeu	t1, s2, printMatrix_end2
 			
 			# save 'row-counter'(t0) and 'col-counter'(t1) to stack
 			addi	sp, sp, -8
 			sw	t0, 0(sp)
 			sw	t1, 4(sp)
 			
-			# initialization arguments for 'getEdge'
+			# initialization arguments for 'getItem'
 			mv	a0, t0		# copy 'row-counter'(t0) to 'a0'
 			mv	a1, t1		# copy 'col-counter'(t1) to 'a1'
 			
-			jal	getEdge		# get edge = [a0][a1]
-			jal	printInt	# print current edge
+			jal	getItem		# get item = [a0][a1]
+			jal	printInt	# print current item
 			
 			# print tab'\t' after item
 			la	a0, str_TAB
@@ -165,7 +181,7 @@ printMatrix:
 
 # 'row-index' in (a0)
 # 'col-index' in (a1)
-printEdge:
+printItem:
 	# save 'return-address' to stack
 	addi	sp, sp, -4
 	sw	ra, 0(sp)
@@ -200,7 +216,7 @@ printEdge:
 	jal	printStr
 	
 	mv	a0, t0			# set 'row-index' in (a0)
-	jal	getEdge			# get 'edge-value' in (a0)
+	jal	getItem			# get 'item-value' in (a0)
 	jal	printInt		# print 'edge-value'
 	
 	# load 'return-address' from stack
